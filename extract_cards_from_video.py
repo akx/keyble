29061ffdf06f6488dd14afdca1a625e255038300
Aarni Koskela <akx@iki.fi>
2020-01-04 12:54:30 +0200

    extract_cards_from_video.py: create destdir

--- extract_cards_from_video.py
+++ extract_cards_from_video.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 from keyble.video_frontend import extract_cards_from_video
 
@@ -8,6 +9,7 @@ def main():
     ap.add_argument('-i', '--input-video', required=True)
     ap.add_argument('-d', '--dest-dir', required=True)
     args = ap.parse_args()
+    os.makedirs(args.dest_dir, exist_ok=True)
     extract_cards_from_video(input_video=args.input_video, dest_dir=args.dest_dir)
 
 
